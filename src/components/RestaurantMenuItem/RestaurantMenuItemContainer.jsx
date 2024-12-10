9de55eee9e936e4885cc0cf40c3bd9a9f39f8336
Pavel Damaskin <falard.damaskin@gmail.com>
2024-12-11 00:26:47 +0200

    feat: solution for lesson 9

--- src/components/RestaurantMenuItem/RestaurantMenuItemContainer.jsx
+++ src/components/RestaurantMenuItem/RestaurantMenuItemContainer.jsx
@@ -1,9 +1,27 @@
 import { useSelector } from 'react-redux'
-import { selectDishById } from '../../redux/entities/dishes-slice'
+import { selectDishById } from '../../redux/entities/dishes/dishes-slice'
 import { RestaurantMenuItem } from './RestaurantMenuItem'
+import { getDishes } from '../../redux/entities/dishes/getDishes'
+import { useRequest } from '../../hooks/useRequest'
 
 export const RestaurantMenuItemContainer = ({ id }) => {
-  const { name } = useSelector((state) => selectDishById(state, id))
+  const requestStatus = useRequest(getDishes)
+
+  const dish = useSelector((state) => selectDishById(state, id))
+
+  if (requestStatus === 'pending') {
+    return 'Loading...'
+  }
+
+  if (requestStatus === 'rejected') {
+    return 'Error'
+  }
+
+  if (!dish) {
+    return null
+  }
+
+  const { name } = dish
 
   return (
     <RestaurantMenuItem
---
+++ src/redux/entities/restaurants/getRestaurants.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { selectRestaurantsIds } from './restaurants-slice'
+
+export const getRestaurants = createAsyncThunk(
+  'restaurants/getRestaurants',
+  async (_, { rejectWithValue }) => {
+    const response = await fetch('http://localhost:3001/api/restaurants')
+    const result = await response.json()
+
+    if (!result.length) {
+      rejectWithValue('restaurants/getRestaurants no data')
+      return
+    }
+
+    return result
+  },
+  {
+    condition: (_, { getState }) => {
+      return selectRestaurantsIds(getState()).length === 0
+    },
+  },
+)
